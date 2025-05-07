0667d42c964dea4604344b65bd87e958d9774415
Razavn-Alexandru <razvan.cioban@proton.me>
2025-05-07 14:37:45 -0400

    rook and bishop movements

--- game/board.py
+++ game/board.py
@@ -1,4 +1,4 @@
-from game.rules import get_pawn_moves, get_knight_moves
+from game.rules import get_pawn_moves, get_knight_moves, get_rook_moves, get_bishop_moves
 from game.move import Move
 
 class ChessBoard:
@@ -39,6 +39,10 @@ class ChessBoard:
             return get_pawn_moves(self.board, row, col, colour, self.en_passant_target)
         if piece_type == "knight":
             return get_knight_moves(self.board, row, col, colour)
+        if piece_type == "rook":
+            return get_rook_moves(self.board, row, col, colour)
+        if piece_type == "bishop":
+            return get_bishop_moves(self.board, row, col, colour)
         
         return []
 
--- game/rules.py
+++ game/rules.py
@@ -40,3 +40,47 @@ def get_knight_moves(board, row, col, colour):
 
     print(f"valid voves: {len(moves)}")
     return moves
+
+def get_rook_moves(board, row, col, colour):
+    moves = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for to_row, to_col in directions:
+        new_row, new_col = row + to_row, col + to_col
+        while 0 <= new_row < 8 and 0 <= new_col < 8:
+            target = board[new_row][new_col]
+            if target == "":
+                moves.append((new_row, new_col)) # empty cell
+            elif target[0] != colour:
+                moves.append((new_row, new_col)) # enemy piece
+                break
+            else:
+                break # friendly piece
+
+            new_row += to_row
+            new_col += to_col
+
+    print(f"valid voves: {len(moves)}")
+    return moves
+
+def get_bishop_moves(board, row, col, colour):
+    moves = []
+    directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    for to_row, to_col in directions:
+        new_row, new_col = row + to_row, col + to_col
+        while 0 <= new_row < 8 and 0 <= new_col < 8:
+            target = board[new_row][new_col]
+            if target == "":
+                moves.append((new_row, new_col)) # empty cell
+            elif target[0] != colour:
+                moves.append((new_row, new_col)) # enemy piece
+                break
+            else:
+                break # friendly piece
+
+            new_row += to_row
+            new_col += to_col
+
+    print(f"valid voves: {len(moves)}")
+    return moves
